c8f405152138b119c4033018161134ae6dc50675
Tamer39 <vladpriwalov08@gmail.com>
2022-03-14 21:26:19 +0200

    Made more comments explaining Player.lua

--- pset10 (final project)/super-mario-idea/Player.lua
+++ pset10 (final project)/super-mario-idea/Player.lua
@@ -1,41 +1,69 @@
 Player = Class:extend()
 
+-- Player:init(image)
 function Player:init(image)
+
+    -- Initialize variables
     self.x = 0
     self.y = 0
     self.dx = 0
     self.size = 'small'
     self.image = love.graphics.newImage(image)
-    self.count = 0
+    self.momentum_factor = 0
     self.moving = false
 end
 
+-- Player:update(dt)
 function Player:update(dt)
 
+    -- If the d key is down at any point
     if love.keyboard.isDown('d') then
+
+        -- Set the moving state to be true
         self.moving = true
-        self.dx = 10 * self.count
+
+        -- Set player's delta x to be 10 pixels * momentum factor
+        self.dx = 10 * self.momentum_factor
+
+        -- Change player position based on delta x
         self.x = self.x + self.dx
     end
 
+    -- If the a key is down at any point
     if love.keyboard.isDown('a') then
+
+        -- Set the moving state to be true
         self.moving = true
-        self.dx = 10 * self.count
+
+        -- Set player's delta x to be 10 pixels * momentum factor
+        self.dx = 10 * self.momentum_factor
+
+        -- Change player position based on delta x
         self.x = self.x - self.dx
     end
 
-    if self.moving == false and self.count > 0 then
-        self.count = self.count - 1 * dt
+    -- If the player isn't moving and his momentum should be reset
+    if self.moving == false and self.momentum_factor > 0 then
+
+        -- Lower down momentum factor each frame
+        self.momentum_factor = self.momentum_factor - 1 * dt
     end
 
+    -- If the spacebar is down at any point
     if love.keyboard.isDown('space') then
     end
 
+    -- If the player is moving
     if self.moving == true then
-        self.count = self.count + 1 * dt
+
+        -- Increment momentum factor each frame
+        self.momentum_factor = self.momentum_factor + 1 * dt
     end
 end
 
+-- Player:draw()
 function Player:draw()
+    
+    -- Draw the image at specified x and y
     love.graphics.draw(self.image, self.x, self.y)
 end
