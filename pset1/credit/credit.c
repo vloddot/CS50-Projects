f7ea178cc77613a1f01bfe18ee77db90c13b7f79
vloddot <vladpriwalov08@gmail.com>
2022-06-18 15:31:57 +0200

    Edit credit (in py and c)

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -4,108 +4,124 @@
 
 int isValid(char *c_num);
 
-int main(void) {
-	// Allocate memory for the credit card number
-	char *c_num = malloc(1024);
-	if (c_num == NULL) {
-		perror("MallocFailError: allocate for c_num\n");
-		return 1;
-	}
+int main(void)
+{
+    // Allocate memory for the credit card number
+    char *c_num = malloc(1024);
+    if (c_num == NULL)
+    {
+        perror("MallocFailError: allocate for c_num\n");
+        return 1;
+    }
 
-	// Get the credit card number
-	printf("Enter your credit card number: ");
-	scanf("%s", c_num);
+    // Get the credit card number
+    printf("Enter your credit card number: ");
+    scanf("%s", c_num);
 
-	// Check if the credit card number is valid
-	if (!isValid(c_num)) {
-		printf("INVALID\n");
-	}
+    // Check if the credit card number is valid
+    if (!isValid(c_num))
+    {
+        printf("INVALID\n");
+    }
 
-	// Find out which credit card company it belongs to
-	else if (
-		strncmp(c_num, "4", 1) == 0
-		&& (strlen(c_num) == 13 || strlen(c_num) == 16)
-	) {
-		printf("VISA\n");
-	} else if (
-		(strncmp(c_num, "51", 2) == 0 ||
-		strncmp(c_num, "52", 2) == 0 ||
-		strncmp(c_num, "53", 2) == 0 ||
-		strncmp(c_num, "54", 2) == 0 ||
-		strncmp(c_num, "55", 2) == 0) &&
-		strlen(c_num) == 16
-	) {
-		printf("MASTERCARD\n");
-	} else if (
-		strncmp(c_num, "34", 2) == 0 ||
-		strncmp(c_num, "37", 2) == 0 &&
-		strlen(c_num) == 15
-	) {
-		printf("AMEX\n");
-	} else {
-		printf("INVALID\n");
-	}
+    // Find out which credit card company it belongs to
+    else if (
+        strncmp(c_num, "4", 1) == 0 && (strlen(c_num) == 13 || strlen(c_num) == 16))
+    {
+        printf("VISA\n");
+    }
+    else if (
+        (strncmp(c_num, "51", 2) == 0 ||
+         strncmp(c_num, "52", 2) == 0 ||
+         strncmp(c_num, "53", 2) == 0 ||
+         strncmp(c_num, "54", 2) == 0 ||
+         strncmp(c_num, "55", 2) == 0) &&
+        strlen(c_num) == 16)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if (
+        strncmp(c_num, "34", 2) == 0 ||
+        strncmp(c_num, "37", 2) == 0 &&
+        strlen(c_num) == 15)
+    {
+        printf("AMEX\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 
-	// Free the memory allocated for the credit card number
-	free(c_num);
-	return 0;
+    // Free the memory allocated for the credit card number
+    free(c_num);
+    return 0;
 }
 
-int isValid(char *c_num) {
-	unsigned long len = strlen(c_num);
-	int sum = 0;
-	int digit;
+int isValid(char *c_num)
+{
+    unsigned long len = strlen(c_num);
+    int sum = 0;
+    int digit;
 
-	// Hans Peter Luhn's algorithm from the Wikipedia page: https://en.wikipedia.org/wiki/Luhn_algorithm
-	/*
-		* 1. Starting from the second-to-last digit, double every other digit
-		* 2. If the doubled value of a digit is greater than 9, add the product's digits together.
-		* 3. Sum the digits
-		* 4. If the sum is a multiple of 10, the number is valid.
-	*/
+    // Hans Peter Luhn's algorithm from the Wikipedia page: https://en.wikipedia.org/wiki/Luhn_algorithm
+    /*
+     * 1. Starting from the second-to-last digit, double every other digit
+     * 2. If the doubled value of a digit is greater than 9, add the product's digits together.
+     * 3. Sum the digits
+     * 4. If the sum is a multiple of 10, the number is valid.
+     */
 
-	// Step 1
-	/*
-	Check if the length of the credit card is odd or even
-	because we need to double every other digit *starting* from the second-to-last digit
-	 and the only way to know that is by checking the length
-	*/
-	// Step 2
-	/*
-		If the length of the credit card is odd, then the second-to-last digit will be on an odd index
-		Else, even index. So if i is even and length is even, we need to double that digit and vice versa.
-		And if the doubled value of a digit is greater than 9, add the product's digits together.
-		And we add all that to the sum.
-	*/
-	// Step 3
-	/*
-		If the sum is a multiple of 10, the number is valid.
-	*/
-	if (len % 2 == 1) {
-		for (int i = 0; i < len; i++) {
-			// Step 2
-			digit = c_num[i] - '0';
-			if (i % 2 == 1) {
-				digit *= 2;
-				if (digit > 9) {
-					// Add the products' digits together
-					digit = (digit % 10) + (digit / 10);
-				}
-			}
-			sum += digit;
-		}
-	} else {
-		for (int i = 0; i < len; i++) {
-			digit = c_num[i] - '0';
-			if (i % 2 == 0) {
-				digit *= 2;
-				if (digit > 9) {
-					digit = (digit % 10) + (digit / 10);
-				}
-			}
-			sum += digit;
-		}
-	}
+    // Step 1
+    /*
+    Check if the length of the credit card is odd or even
+    because we need to double every other digit *starting* from the second-to-last digit
+     and the only way to know that is by checking the length
+    */
+    // Step 2
+    /*
+        If the length of the credit card is odd, then the second-to-last digit will be on an odd index
+        Else, even index. So if i is even and length is even, we need to double that digit and vice versa.
+        And if the doubled value of a digit is greater than 9, add the product's digits together.
+        And we add all that to the sum.
+    */
+    // Step 3
+    /*
+        If the sum is a multiple of 10, the number is valid.
+    */
+    if (len % 2 == 1)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            // Step 2
+            digit = c_num[i] - '0';
+            if (i % 2 == 1)
+            {
+                digit *= 2;
+                if (digit > 9)
+                {
+                    // Add the products' digits together
+                    digit = (digit % 10) + (digit / 10);
+                }
+            }
+            sum += digit;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < len; i++)
+        {
+            digit = c_num[i] - '0';
+            if (i % 2 == 0)
+            {
+                digit *= 2;
+                if (digit > 9)
+                {
+                    digit = (digit % 10) + (digit / 10);
+                }
+            }
+            sum += digit;
+        }
+    }
 
-	return sum % 10 == 0;
+    return sum % 10 == 0;
 }
--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -1,71 +1,59 @@
-def validate_card(card):
-    multiplied = 0
-    added = 0
-    for i in range(len(card)):
-        if i % 2 == 0:
-            added += int(card[i])
-        else:
-            # Multiply the values of card[i] by 2 and put them in multiplies[i]
-            multiplied = int(card[i]) * 2
-
-            # If the multiplied number is double digits, add its products and NOT the number, and same thing will work if it's not double digits
-
-            multiplied = str(multiplied)
-            for j in range(len(multiplied)):
-                added += int(multiplied[j])
-
-    added = str(added)
-    # Checks if the second value in the added value is 0, if so return true
-    if added[1] == '0':
-        return True
+import sys
 
 
-    return False
-
-
-def card_check(card):
-
-    # Checks if the card is a valid AMEX card, if not it goes onto the next card check
-    if len(card) == 15 and card[0] == '3' and (card[1] == '4' or card[1] == '7'):
-        return "AMEX"
-
-    # Checks if the card is a valid MasterCard, if not it goes onto the next card check
-    if len(card) == 16 and card[0] == '5' and (card[1] == '1' or card[1] == '2' or card[1] == '3' or card[1] == '4' or card[1] == '5'):
-        return "MASTERCARD"
-
-    # Checks if the card is a valid Visa card, if not it goes onto the next card check
-    if (len(card) == 13 or len(card) == 16) and card[0] == '4':
-        return "VISA"
-
-    return False
 def main():
-    # Ask user for input, which is the credit card number
-    try:
-        card = input("Credit card number: ").strip()
+    c_num = input("Enter a credit card number: ").strip()
+    if not is_valid(c_num):
+        print('INVALID')
+        sys.exit(1)
+        
+    if c_num[0] == '4' and (len(c_num) == 16 or len(c_num) == 13):
+        print('VISA')
+        
+    elif (c_num[:2] == '51' or \
+        c_num[:2] == '52' or \
+        c_num[:2] == '53' or \
+        c_num[:2] == '54' or \
+        c_num[:2] == '55') and len(c_num) == 16:
 
-    # In case the user writes a string or anything other than an integer, print INVALID and return, effectively quitting the program
-    except ValueError:
-        print("INVALID")
-        return
+        print('MASTERCARD')
+        
+    elif c_num[:2] == '34' or c_num[:2] == '37' and len(c_num) == 15:
+        print('AMEX')
+        
+    else:
+        print('INVALID')
 
 
-    # Check for if the card is valid or not by calling the validate_card function
-    result = validate_card(card)
 
-    # If the result is false, print INVALID and return, effectively quitting the program
-    if result == False:
-        print("INVALID")
-        return
+def is_valid(c_num):
+    c_sum = 0
+    if len(c_num) % 2 == 1:
 
-    # Checks for if the card has the same patterns as a VISA, MASTERCARD, or AMEX card
-    result = card_check(card)
+        try:
+            for i, digit in enumerate(map(int, c_num)):
+                if i % 2 == 1:
+                    digit *= 2
+                    if digit > 9:
+                        digit = (digit % 10) + (digit // 10)
 
-    # If the result is false, print INVALID
-    if result == False:
-        print("INVALID")
-        return
 
-    # Print the result returned by card_check(card) if it's not false
-    print(result)
-# Calling main
-main()
+                c_sum += digit
+        except ValueError:
+            return False
+    else:
+        for i, digit in enumerate(map(int, c_num)):
+            if i % 2 == 0:
+                digit *= 2
+                if digit > 9:
+                    digit = (digit % 10) + (digit // 10)
+
+
+            c_sum += digit
+
+
+    return c_sum % 10 == 0
+
+
+if __name__ == '__main__':
+    main()
